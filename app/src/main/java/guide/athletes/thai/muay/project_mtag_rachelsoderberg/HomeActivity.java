a3d6022d044104a442e06521b3eb79ce1d40b36e
rsoderberg <r_soderberg@yahoo.com>
2017-11-20 09:03:44 -0800

    Initial commit

---
+++ app/src/main/java/guide/athletes/thai/muay/project_mtag_rachelsoderberg/HomeActivity.java
@@ -0,0 +1,13 @@
+package guide.athletes.thai.muay.project_mtag_rachelsoderberg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
