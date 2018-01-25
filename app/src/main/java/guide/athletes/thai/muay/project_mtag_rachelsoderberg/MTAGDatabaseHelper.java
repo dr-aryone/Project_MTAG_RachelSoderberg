1f3bb6555552c032406eb21ee01274b8b7c43515
rsoderberg <r_soderberg@yahoo.com>
2018-01-25 10:59:06 -0800

    Revert "Added to Database Functionality"
    This reverts commit 799fe27ce5a5f7dbc95e1eec217b4e7c893566ab.

--- app/src/main/java/guide/athletes/thai/muay/project_mtag_rachelsoderberg/MTAGDatabaseHelper.java
+++ app/src/main/java/guide/athletes/thai/muay/project_mtag_rachelsoderberg/MTAGDatabaseHelper.java
@@ -16,55 +16,24 @@ class MTAGDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // When database is first called
-        updateDatabase(db, 0, DB_VERSION);
+
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Update database when version number increases
-        updateDatabase(db, oldVersion, newVersion);
+
     }
 
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Update database when version number decreases
-        updateDatabase(db, oldVersion, newVersion);
     }
 
-    private static void insertUser(SQLiteDatabase db, String username, String email, String firstname, String lastname, String country) {
-        ContentValues userValues = new ContentValues();
-        userValues.put("USERNAME", username);
-        userValues.put("EMAIL", email);
-        userValues.put("FIRSTNAME", firstname);
-        userValues.put("LASTNAME", lastname);
-        userValues.put("COUNTRY", country);
-        db.insert("USER", null, userValues);
-    }
+    // private static void insertObject(SQLiteDatabase db, String name, String description, int resourceId) {}
 
-    private static void insertAccount(SQLiteDatabase db, String username, String password) {
-        ContentValues accountValues = new ContentValues();
-        accountValues.put("USERNAME", username);
-        accountValues.put("PASSWORD", password);
-    }
-
-     private void updateDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
-          if (oldVersion < 1) {
-              // Create User table
-              db.execSQL("CREATE TABLE USER ("
-              + "_id PRIMARY KEY AUTOINCREMENT, "
-              + "USERNAME TEXT, "
-              + "EMAIL TEXT, "
-              + "FIRSTNAME TEXT, "
-              + "LASTNAME TEXT, "
-              + "COUNTRY TEXT);");
-              // Populate User table data (for testing)
-              insertUser(db, "CL1FF72", "cliffsemail@hotmail.com", "Cliff", "Cliffton", "USA");
-
-              // Create Account info table
-              db.execSQL("CREATE TABLE ACCOUNT ("
-              + "_id PRIMARY KEY AUTOINCREMENT, "
-              + "USERNAME TEXT, "
-              + "PASSWORD TEXT);");
-              // Populate Account table data (for testing)
-              insertAccount(db, "CL1FF72", "123abc");
-          }
-     }
+    // private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+    //      if (oldVersion < 1) {
+    //           Create empty object table
+    //           Insert objects, each in a separate row
+    //      }
+    // }
 }
