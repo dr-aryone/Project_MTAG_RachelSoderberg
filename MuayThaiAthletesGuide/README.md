d1705b6ee51a69aef8a5a30b50c7d1b629749fc7
rsoderberg <r_soderberg@yahoo.com>
2018-02-15 14:41:07 -0800

    Timer & Spinner
    - Added Excel spreadsheet for reporting and managing bugs
    - Updated Readme file
    - Improved Spinner code by changing to a switch statement
    - Added error checking in Spinner for unexpected results
    - Added functions for each timer type (most currently stubbed out
    Bugs: Recorded in new Bug Report Excel spreadsheet.

--- MuayThaiAthletesGuide/README.md
+++ MuayThaiAthletesGuide/README.md
@@ -1,4 +1,5 @@
 # The Muay Thai Athlete's Guide (Project MTAG) - Rachel Soderberg
+##### Fall 2017 - Spring 2018  
 ###### Project MTAG is an Android application created to enhance the under-developed app store niche for Muay Thai athletes and practitioners. The Muay Thai Athlete's Guide provides the user with a comprehensive list of Muay Thai combos and techniques in an easy to read reference format.  
 ###### The user is met with an extensive list of new options such as creation and saving of user-submitted combos, tutorial videos for instructional lessons, workouts to be done in a typical Muay Thai or general boxing gym, and timers to allow for a seamless workout session.  
 ---  
@@ -13,15 +14,22 @@
 "As a busy athlete, I only have so much time in the gym. I want an app where I can look up what I want quickly and without a lot of fuss."  
 "As a parent of a child who trains, I want an app that will provide instruction and information in a manner that is safe and accessible to all ages."  
   
-##### Testing  
+##### Testing & Devices  
 ###### Android Emulator APIs Used:  
 - Nexus 5X API 26  
 - Galaxy Nexus API 27  
+
+###### Handheld Devices Used:
+- Samsung Galaxy S5
   
 ##### Error Handling  
 ###### Login and Registration:  
 - Empty email address, password  
 - Email address, password too short/long  
 - Email address invalid (requires @ and .com)  
-- Email address associated with account, require registration if not
-- Password associated with login email is correct to log in
+- Email address associated with account, require registration if not  
+- Password associated with login email is correct to log in  
+  
+###### Timer:  
+- Unexpected results for Timer Selection spinner  
+- Catch timer before value exceeds 99:59:59 or 00:00:00 and throw error via toast  
--- MuayThaiAthletesGuide/app/src/main/java/com/mtag/app/muaythaiathletesguide/TimerActivity.java
+++ MuayThaiAthletesGuide/app/src/main/java/com/mtag/app/muaythaiathletesguide/TimerActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -15,17 +17,12 @@ import java.util.Locale;
 
 // Gradient Buttons: https://github.com/sapandiwakar/PSGradientButtons
 
-// Basic Stopwatch: Count from 0:00 to 99:59 (or cap)
-// Countdown: Count from 99:59 (or cap) to 0:00
-// Tabata: Beep every 20th and 30th second
-//         Reset to 0:00 on each 30th second
-// Fight Gone Bad: 17min cap, beep on each minute
-// "3 On 1 Off": Beep every 3rd and 4th minute
-
-public class TimerActivity extends Activity {
+public class TimerActivity extends Activity implements AdapterView.OnItemSelectedListener {
     private int seconds = 0; // Number of seconds passed
     private boolean running; // Check whether timer is running
     private boolean wasRunning;
+
+    private int timeCap = 0; // Time cap, when applicable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +38,8 @@ public class TimerActivity extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
 
         // Restore activity's state by getting values from Bundle
         if (savedInstanceState != null) {
@@ -48,7 +47,59 @@ public class TimerActivity extends Activity {
             running = savedInstanceState.getBoolean("running");
             wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
-        runBasicTimer(); // Start stopwatch when method is created
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+        String selection = parent.getItemAtPosition(pos).toString();
+
+        // TODO: Remove Toast outputs after testing
+        // Call Timer types when corresponding position is chosen
+        switch(pos) {
+            case 0: // Basic Stopwatch: Count from 0:00:00 to 99:59:59 (or cap)
+                running = false; // Stop clock
+                seconds = 0; // Reset seconds to zero
+                runBasicTimer();
+                break;
+            case 1: // Countdown: Count from 99:59:59 (or cap) to 0:00:00
+                Toast.makeText(parent.getContext(), "Selected: " + selection, Toast.LENGTH_LONG).show();
+                running = false;
+                seconds = 1200; // Default cap 20:00:00
+                runCountdownTimer();
+                break;
+            case 2: // Tabata: Beep every 20th and 30th second. Reset to 0:00:00 on each 30th second
+                Toast.makeText(parent.getContext(), "Selected: " + selection, Toast.LENGTH_LONG).show();
+                running = false;
+                seconds = 0;
+                runTabataTimer();
+                break;
+            case 3: // Fight Gone Bad: 17min cap, beep on each minute
+                Toast.makeText(parent.getContext(), "Selected: " + selection, Toast.LENGTH_LONG).show();
+                running = false;
+                seconds = 0;
+                runFGBTimer();
+                break;
+            case 4: // "3 On 1 Off": Beep every 3rd and 4th minute
+                Toast.makeText(parent.getContext(), "Selected: " + selection, Toast.LENGTH_LONG).show();
+                running = false;
+                seconds = 0;
+                runThreeOneTimer();
+                break;
+            case 5: // "5 On 1 Off": Beep every 5th and 6th minute
+                Toast.makeText(parent.getContext(), "Selected: " + selection, Toast.LENGTH_LONG).show();
+                running = false;
+                seconds = 0;
+                runFiveOneTimer();
+                break;
+            default:
+                running = false;
+                seconds = 0;
+                Toast.makeText(parent.getContext(), "Error", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent){
+        // Another interface callback
     }
 
     @Override
@@ -84,7 +135,6 @@ public class TimerActivity extends Activity {
     }
 
     public void onClickReset(View view) {
-        running = false; // Stop stopwatch
         seconds = 0; // Reset seconds to zero
     }
 
@@ -105,9 +155,58 @@ public class TimerActivity extends Activity {
                 if (running) {
                     seconds++;
                 }
+                // Don't allow timer to go over 99:59:59
+                if (seconds >= 359999) {
+                    running = false;
+                    Toast.makeText(getApplicationContext(), "Maximum time reached", Toast.LENGTH_LONG).show();
+                }
                 // Post code again with delay of one second
                 handler.postDelayed(this, 1000);
             }
         });
+    }
+
+    private void runCountdownTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                // Format time to hours, minutes, and seconds
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+
+                if (running) {
+                    seconds--;
+                }
+                // Don't allow timer to go under 0:00:00
+                if (seconds <= 1) {
+                    running = false;
+                    Toast.makeText(getApplicationContext(), "Maximum time reached", Toast.LENGTH_LONG).show();
+                }
+                // Post code again with delay of one second
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    private void runTabataTimer() {
+
+    }
+
+    private void runFGBTimer() {
+
+    }
+
+    private void runThreeOneTimer() {
+
+    }
+
+    private void runFiveOneTimer() {
+
     }
 }
